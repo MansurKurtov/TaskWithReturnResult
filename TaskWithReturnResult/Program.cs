efd6cada181982fb43c07a7af05647518350c3c4
Mansur <kurtov.mansur@gmail.com>
2021-09-26 17:09:55 +0500

    Add project files.

---
+++ TaskWithReturnResult/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TaskWithReturnResult
+{
+    /// <summary>
+    /// The task can return a result. There is no direct mechanism to return the result from a thread.
+    /// </summary>
+    class Program
+    {
+        private static IList<Rectangle> _rectangles;
+        static void Main(string[] args)
+        {
+            _rectangles = new List<Rectangle>()
+            {
+                new Rectangle(10,15),
+                new Rectangle(6,14),
+                new Rectangle(18,45),
+                new Rectangle(3,8),
+                new Rectangle(20,9),
+                new Rectangle(30,4),
+                new Rectangle(40,17),
+                new Rectangle(3,25),
+                new Rectangle(2,35),
+                new Rectangle(1,28)
+            };
+
+
+            var task = new Task<Rectangle>(() => GetMaxPeremetrRect());
+            task.Start();
+            task.Wait();
+            System.Console.WriteLine(task.Result.Peremetr());
+            System.Console.ReadLine();
+
+        }
+
+        private static Rectangle GetMaxPeremetrRect()
+        {
+            if (_rectangles.Count == 0)
+                return default;
+
+            var result = _rectangles[0];
+            foreach (var item in _rectangles)
+            {
+                if (result.Peremetr() < item.Peremetr())
+                    result = item;
+            }
+
+            return result;
+        }
+        
+    }
+    public class Rectangle
+    {
+        private double width;
+        private double height;
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public double Peremetr()
+        {
+            return 2 * (width + height);
+        }
+    }
+}
